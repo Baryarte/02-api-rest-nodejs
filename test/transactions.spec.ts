bd51dd1c51a84a831964aca73f4e1784e79e70c7
Thiaggo Ferreira <thiaggo98@gmail.com>
2023-11-02 19:25:46 -0300

    Teste de listagem de transações

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -20,5 +20,30 @@ describe('Transactions routes', () => {
     })
 
     expect(response.statusCode).toEqual(201)
+    console.log(response.headers)
+  })
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('set-cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 5000,
+      }),
+    ])
   })
 })
