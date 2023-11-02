e7d2d06dc3940f6bedd3da90d7f4648405fe7b84
Thiaggo Ferreira <thiaggo98@gmail.com>
2023-11-02 19:06:46 -0300

    Adicionando testes

---
+++ test/transactions.spec.ts
@@ -0,0 +1,24 @@
+﻿import { afterAll, beforeAll, expect, it } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app'
+import { describe } from 'node:test'
+
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('user can create a new transaction', async () => {
+    const response = await request(app.server).post('/transactions').send({
+      title: 'New transaction',
+      amount: 5000,
+      type: 'credit',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
